963c244eced17dc35b7131a819069a263d44723a
MrYuriiKravchenko <MisterKrava@yandex.ru>
2024-07-26 20:42:18 +0300

    first

---
+++ ratemyreads/users/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='users_images', blank=True, null=True)
+
+    class Meta:
+        db_table = 'user'
+
+        def __str__(self):
+            return self.username
